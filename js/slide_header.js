f72d5f0218b0c6ed1bae91d4cba02e35fa25ddc1
unknown <elongajosue22@gmail.com>
2022-04-14 12:02:17 +0000

    Upload version finale

---
+++ js/slide_header.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+    $('header .owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        smartSpeed: 1500,
+        autoplayHoverPause: true,
+    });
+
+    $('#el_gallery .owl-carousel').owlCarousel({
+        items: 3,
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        smartSpeed: 1500,
+        autoplayHoverPause: true,
+        animateOut: true
+    });
+});
